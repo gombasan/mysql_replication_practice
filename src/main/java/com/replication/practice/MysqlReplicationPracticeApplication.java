6f04cf73f69398693d015c4418d45019e80ea2d7
thhan <thhan@weallys.com>
2024-01-09 16:53:34 +0900

    project_init_commit

---
+++ src/main/java/com/replication/practice/MysqlReplicationPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.replication.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlReplicationPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlReplicationPracticeApplication.class, args);
+	}
+
+}
