9261144f16fe20b202ffe3abe0c7a42f14ae3d41
gombasan <xogmlwkdi@naver.com>
2024-01-11 16:07:27 +0900

    feat : Spring Data Jpa 설정 추가 및 동적 DataSource 설정 추가
    - DataSource 를 동적으로 생성하도록 변경.
    (`LazyConnectionDataSourceProxy` 로 생성하여 실제로 쿼리가 실행 될 때, DataSource 를 실행)
    - 동적으로 생성된 DataSource 를 빈 객체로 생성한다.
    - `DataSourceAutoConfiguration` 을 사용하지 않으므로, JPA 설정을 Properties 파일에 설정을 읽어와 EntityManager 를 생성함.

--- src/main/java/com/replication/practice/db/config/MultiDataSourceConfig.java
+++ src/main/java/com/replication/practice/db/config/MultiDataSourceConfig.java
@@ -1,27 +1,86 @@
 package com.replication.practice.db.config;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.AbstractJpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import com.replication.practice.db.MultiDataSource;
+import com.replication.practice.db.ReplicationRoutingDataSource;
+import com.zaxxer.hikari.HikariDataSource;
 
+import jakarta.persistence.EntityManagerFactory;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Configuration
 @Slf4j
-// @RequiredArgsConstructor
+@RequiredArgsConstructor
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class}) // DataSource 자동 설정을 제외시킨다.
 @EnableConfigurationProperties(MultiDataSource.class)
 public class MultiDataSourceConfig {
 	private final MultiDataSource multiDataSource;
+	private final JpaProperties jpaProperties;
 
-	public MultiDataSourceConfig(MultiDataSource multiDataSource) {
-		this.multiDataSource = multiDataSource;
-		log.info("username = {}", multiDataSource.getUsername());
-		log.info("password = {}", multiDataSource.getPassword());
-		log.info("slave1 Name = {}" ,multiDataSource.getSlave().get("slave1").getName());
-		log.info("slave1 url = {}", multiDataSource.getSlave().get("slave1").getUrl());
+	public DataSource createDataSource(String url) {
+		return DataSourceBuilder.create()
+			.type(HikariDataSource.class)
+			.url(url)
+			.driverClassName("com.mysql.cj.jdbc.Driver")
+			.username(multiDataSource.getUsername())
+			.password(multiDataSource.getPassword())
+			.build();
+	}
+
+	@Bean
+	public DataSource dataSource() {
+		return new LazyConnectionDataSourceProxy(routingDataSource());
+	}
+
+	@Bean
+	public DataSource routingDataSource() {
+		DataSource defalutDataSource = createDataSource(multiDataSource.getUrl());
+		MultiDataSource.Slave slave = multiDataSource.getSlave().get("slave1");
+		Map<Object, Object> targetDataSource = new HashMap<>();
+		targetDataSource.put("master", defalutDataSource);
+		targetDataSource.put(slave.getName(), createDataSource(slave.getUrl()));
+		ReplicationRoutingDataSource replicationRoutingDataSource = new ReplicationRoutingDataSource();
+		replicationRoutingDataSource.setDefaultTargetDataSource(defalutDataSource);
+		replicationRoutingDataSource.setTargetDataSources(targetDataSource);
+
+		return replicationRoutingDataSource;
+	}
+
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		EntityManagerFactoryBuilder entityManagerFactoryBuilder = createEntityManagerFactoryBuilder(jpaProperties);
+		return entityManagerFactoryBuilder.dataSource(dataSource()).packages("com.replication.practice").build();
+	}
+
+	private EntityManagerFactoryBuilder createEntityManagerFactoryBuilder(JpaProperties jpaProperties) {
+		AbstractJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		return new EntityManagerFactoryBuilder(vendorAdapter, jpaProperties.getProperties(), null);
+	}
+
+	@Bean
+	public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+		JpaTransactionManager tm = new JpaTransactionManager();
+		tm.setEntityManagerFactory(entityManagerFactory);
+		return tm;
 	}
 }
